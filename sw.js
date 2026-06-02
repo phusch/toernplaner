0004aa916b6ccc36d9c1f39c698178562c5f100d
phusch <phuschka@googlemail.com>
2026-06-02 11:04:54 +0200

    V2 mit umgestellten Bereichen

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'chillout-pirates-kosten-v6-layout';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './assets/logo.png',
+  './assets/icon-180.png',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
